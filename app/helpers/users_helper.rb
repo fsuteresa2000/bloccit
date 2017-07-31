3b61402c19f6012cd58e02059f98dfb161e4c186
fsuteresa2000 <fsuteresa2000@yahoo.com>
2017-07-31 15:48:55 -0500

    assignment-32-public-profiles-complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def user_performed_action?(user_action, action_string)
+    if user_action.empty?
+      render html: "#{@user.name} has not created any #{action_string.pluralize} yet.".html_safe
+    else
+      render html: "<h2>#{action_string.capitalize.pluralize}</h2> #{render user_action}".html_safe
+    end
+  end
 end
